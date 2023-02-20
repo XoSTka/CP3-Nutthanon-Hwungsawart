9ae90f79ed73bc13240585865c373a045b1cbcbf
PpNutthanon <nutthanon.hw@gmail.com>
2023-02-20 10:23:52 +0700

    Create Exercise4_Nutthanon_H.py

---
+++ Exercise4_Nutthanon_H.py
@@ -0,0 +1,10 @@
+#Program Display Score Year2 Semester1
+Foundation_English               = 56.5
+General_Business                 = 76.8
+Introduction_to_Computer_Systems = 82.0 
+Computer_Programming             = 77.7
+print("---------------- Your Score ----------------")
+print("Foundation English               :",Foundation_English)
+print("General Business                 :", General_Business)
+print("Introduction to Computer Systems :", Introduction_to_Computer_Systems)
+print("Computer Programming             :", Computer_Programming)
