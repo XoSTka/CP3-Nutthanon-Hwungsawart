18786e752fcc94dfaeacbb0041aee11efbbb5037
PpNutthanon <nutthanon.hw@gmail.com>
2023-02-21 20:36:44 +0700

    ---

---
+++ Exercise5_1_Nutthanon_H.py
@@ -0,0 +1,6 @@
+number1 = int(input("Enter the first number :"))
+number2 = int(input("Enter the second number :"))
+print(number1,"+",number2,"=",number1+number2)
+print(number1,"-",number2,"=",number1-number2)
+print(number1,"*",number2,"=",number1*number2)
+print(number1,"/",number2,"=",number1/number2)
